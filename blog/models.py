f212963f61c267bdc6f3e7676cd3269740f1efd0
sonmezmusa <sonmezmusa@outlook.com>
2020-12-12 19:46:09 +0300

    Sitede gönderi oluşturmak ve düzenlemek için view'ler eklendi.

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,8 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    title = models.CharField(max_length=200, verbose_name='Başlık')
+    text = models.TextField(verbose_name='İçerik')
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
